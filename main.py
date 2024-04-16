5f4b35d87e99d7db5356504e17df13a614a160e8
AleksandraKurzyk <163436034+AleksandraKurzyk@users.noreply.github.com>
2024-04-16 17:49:40 +0200

    wip:code refactor and menu

---
+++ main.py
@@ -0,0 +1,13 @@
+from models.data_source import users
+from utils.crud import read_friends
+
+if __name__=='__main__':
+    while True:
+        print("Welcome to the menu choose an option: ")
+        print("0. Exit ")
+        print("1. Read a list of friends ")
+        menu_option = input("Choose an option:")
+        if menu_option == "0":
+            break
+        if menu_option == "1":
+            read_friends(users)

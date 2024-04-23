9ce7a39d9722d33fb6697e6cdc282577233577ac
AleksandraKurzyk <163436034+AleksandraKurzyk@users.noreply.github.com>
2024-04-23 16:56:49 +0200

    wip:add update function

--- notatnik.py
+++ notatnik.py
@@ -5,5 +5,14 @@ users: list = [
     {"name": "Adrian", "surname": "Dobrzański", "posts": 3, },
     {"name": "Bartek", "surname": "Wyrzykowski", "posts": 666, }
 ]
+def update_user(users: list):
+    imie = input("Podaj imie użytkownika, którego dane chcesz zmienić: ")
+    for user in users:
+        if user["name"] == imie:
 
+            user["name"] = input("Podaj nowe imię: ")
+            user["surname"] = input("Podaj nowe nazwisko: ")
+            user["posts"] = int(input("Podaj nową liczbę postów: "))
 
+update_user(users)
+print(users)

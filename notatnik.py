7fa6cbb72b2a2accec4df12e3a4a686ba56cb3fe
AleksandraKurzyk <163436034+AleksandraKurzyk@users.noreply.github.com>
2024-05-13 10:10:18 +0200

    feat: add new marker, zoom to marker

--- notatnik.py
+++ notatnik.py
@@ -1,18 +1,25 @@
-users: list = [
+import requests
+from bs4 import BeautifulSoup
 
-    {"name": "Julia", "surname": "Gotowiec", "posts": 1500, },
-    {"name": "Hubert", "surname": "Sybilski", "posts": 123456, },
-    {"name": "Adrian", "surname": "Dobrzański", "posts": 3, },
-    {"name": "Bartek", "surname": "Wyrzykowski", "posts": 666, }
-]
-def update_user(users: list):
-    imie = input("Podaj imie użytkownika, którego dane chcesz zmienić: ")
-    for user in users:
-        if user["name"] == imie:
 
-            user["name"] = input("Podaj nowe imię: ")
-            user["surname"] = input("Podaj nowe nazwisko: ")
-            user["posts"] = int(input("Podaj nową liczbę postów: "))
+class User:
+    def __init__(self, name, surname, posts, location):
+        self.name = name
+        self.surname = surname
+        self.posts = posts
+        self.location = location
+        self.wspolrzedne = User.wspolrzedne(self)
 
-update_user(users)
-print(users)
+    def wspolrzedne(self)->list:
+        url: str = f'https://pl.wikipedia.org/wiki/{self.location}'
+        response = requests.get(url)
+        response_html = BeautifulSoup(response.text, 'html.parser')
+        latitude = response_html.select('.latitude')[1].text.replace(",", ".")
+        longitude = response_html.select('.longitude')[1].text.replace(",", ".")
+        return [latitude, longitude]
+
+# https://pl.wikipedia.org/wiki/Kobyłka
+
+Ola = User(name="Ola", surname="Kurzyk", posts="366", location="Lublin")
+
+print(type(Ola.wspolrzedne[0]))
